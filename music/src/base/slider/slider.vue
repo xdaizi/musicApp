3bbcdf82e842c4eec2fe09ea2530af399c027379
xdaizi <1055671428@qq.com>
2018-04-08 14:49:13 +0800

    完善better-scroll为基础的轮播图组件

--- music/src/base/slider/slider.vue
+++ music/src/base/slider/slider.vue
@@ -1,10 +1,12 @@
 <!--轮播图组件  -->
 <template>
   <div class="slider" ref="slider">
-      <div class="slider-group" ref="sliderGroup">
+      <div class="slider-group" ref="sliderGroup" @touchstart = '_cancelAutoPlay'>
           <slot></slot>
       </div>
-      <div class="dots"></div>
+      <div class="dots">
+          <span class="dot" v-for="(item, index) in dots" :key="item" :class="{active:index === currentPageIndex}"></span>
+      </div>
   </div>
 </template>
 
@@ -29,7 +31,7 @@ export default {
         // 间隔时间
         interval: {
             type: Number,
-            default: 4000
+            default: 2000
         },
         // 运动事件
         speed: {
@@ -37,16 +39,37 @@ export default {
             default: 400
         }
     },
+    data() {
+        return {
+            // dots的数组
+            dots: [],
+            // 当前展示页
+            currentPageIndex: 0
+        }
+    },
     mounted() {
         // 初始化轮播图
         this.$nextTick(() => {
             this._setSliderWidth()
+            this._initDots()
             this._initSlider()
+            if (this.autoPlay) {
+                this._play()
+            }
+        })
+        // 当改变窗口大小时,要重新计算
+        window.addEventListener('resize', () => {
+            if (!this.slider) {
+                return
+            }
+            this._setSliderWidth(true)
+            // refresh:重新计算 better-scroll，当 DOM 结构发生变化的时候务必要调用确保滚动的效果正常。
+            this.slider.refresh()
         })
     },
     methods: {
         // 初始化轮播图得宽度
-        _setSliderWidth() {
+        _setSliderWidth(isResize) {
             this.children = this.$refs.sliderGroup.children
             let width = 0
             let sliderWidth = this.$refs.slider.clientWidth
@@ -58,12 +81,16 @@ export default {
                 // 容器总宽 n * sliderWidth
                 width += sliderWidth
             }
-            // 如果为自动轮播,前后需要各克隆一个item
-            if (this.loop) {
+            // 如果为自动轮播,前后需要各克隆一个item,改变窗口大小时不需要
+            if (this.loop && !isResize) {
                 width += 2 * sliderWidth
             }
             // 设置轮播容器得总宽度
             this.$refs.sliderGroup.style.width = width + 'px'
+        },
+        // 初始化dots的个数,_initSlider会克隆元素,所以要在其之前初始化
+        _initDots() {
+            this.dots = new Array(this.children.length)
         },
         // 初始化轮播图得配置
         _initSlider() {
@@ -77,21 +104,38 @@ export default {
                 // 关闭惯性
                 snap: {
                     loop: this.loop,
-                    // 循环轮播
-                    autoPlay: this.autoPlay,
-                    // 自动轮播
-                    interval: this.interval,
                     // 轮播间隔
                     threshold: 0.3,
                     speed: this.speed
                     // 轮播的速度
                 },
-                click: true,
                 bounce: false,
                 // 滚动超过便的回弹动画
                 stopPropagation: true
                 // 阻止冒泡
             })
+            // scrollEnd:缓动结束是触发
+            this.slider.on('scrollEnd', () => {
+                // getCurrentPage() 获得当前展示页信息
+                let pageIndex = this.slider.getCurrentPage().pageX
+                this.currentPageIndex = pageIndex
+                // console.log(pageIndex)
+                if (this.autoPlay) {
+                    clearTimeout(this.timer)
+                    this._play()
+                }
+            })
+        },
+        // 自动播放
+        _play() {
+            this.timer = setTimeout(() => {
+                // next() 下一页
+                this.slider.next(this.speed)
+            }, this.interval)
+        },
+        _cancelAutoPlay() {
+            // 手动滑动时取消自动效果
+            clearTimeout(this.timer)
         }
     }
 }
@@ -139,7 +183,7 @@ export default {
                 &.active {
                     width: 20px;
                     border-radius: 5px;
-                    background: @color-text-ll;
+                    background: @color-theme-d;
                 }
             }
         }
