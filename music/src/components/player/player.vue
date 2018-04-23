057f07a3eed94f02d75ddbf175f69927e59ec98f
xdaizi <1055671428@qq.com>
2018-04-23 22:22:42 +0800

    完成播放器的基础动画效果

--- music/src/components/player/player.vue
+++ music/src/components/player/player.vue
@@ -1,7 +1,13 @@
 <!-- 播放器组件 -->
 <template>
     <div class="player" v-if="playList.length">
-        <transition name="normal">
+        <!-- 动画钩子 -->
+        <transition name="normal"
+            @enter="enter"
+            @after-enter="afterEnter"
+            @leave="leave"
+            @after-leave="afterLeave"
+        >
             <div class="normal-player" v-show="fullScreen">
                 <div class="background">
                     <img width="100%" height="100%" :src="currentSong.image">
@@ -15,7 +21,7 @@
                 </div>
                 <div class="middle">
                     <div class="middle-l">
-                        <div class="cd-wrapper">
+                        <div class="cd-wrapper" ref="cdWrapper">
                             <div class="cd">
                                 <img class="image" :src="currentSong.image">
                             </div>
@@ -88,6 +94,7 @@
 
 <script>
 import { mapGetters, mapMutations } from 'vuex'
+import animations from 'create-keyframe-animation'
 export default {
     computed: {
         ...mapGetters([
@@ -102,6 +109,59 @@ export default {
         },
         open() {
             this.setFullScreen(true)
+        },
+        // el: dom,done:下一步动作的回调
+        enter(el, done) {
+            const {x, y, scale} = this._getPosAndScale()
+            // 定义动画
+            let animation = {
+                0: {
+                    transform: `translate3d(${x}px,${y}px,0) scale(${scale})`
+                },
+                60: {
+                    transform: `translate3d(0,0,0) scale(1.1)`
+                },
+                100: {
+                    transform: `translate3d(0,0,0) scale(1)`
+                }
+            }
+            // 注册动画
+            animations.registerAnimation({
+                name: 'move',
+                animation,
+                presets: {
+                    duration: 400,
+                    easing: 'linear'
+                }
+            })
+            animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+        },
+        afterEnter() {
+            // 注销动画
+            animations.unregisterAnimation('move')
+            // 恢复样式
+            this.$refs.cdWrapper.style.animation = ''
+        },
+        leave(el, done) {
+            this.$refs.cdWrapper.style.transition = 'all 0.4s'
+            const {x, y, scale} = this._getPosAndScale()
+            this.$refs.cdWrapper.style['transform'] = `translate3d(${x}px,${y}px,0) scale(${scale})`
+            this.$refs.cdWrapper.addEventListener('transitionend', done)
+        },
+        afterLeave() {
+            this.$refs.cdWrapper.style.transition = ''
+            this.$refs.cdWrapper.style['transform'] = ''
+        },
+        _getPosAndScale() {
+            const targetWidth = 40
+            const paddingLeft = 40
+            const paddingBottom = 30
+            const paddingTop = 80
+            const width = window.innerWidth * 0.8
+            const scale = targetWidth / width
+            const x = -(window.innerWidth / 2 - paddingLeft)
+            const y = window.innerHeight - paddingTop - width / 2 - paddingBottom
+            return {x, y, scale}
         },
         ...mapMutations({
             setFullScreen: 'SET_FULL_SCREEN'
