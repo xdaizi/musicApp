ede1329287ca693bf4fed24051d65a1defc636a9
xdaizi <1055671428@qq.com>
2018-05-05 18:10:22 +0800

    完成编译打包

--- music/src/router/index.js
+++ music/src/router/index.js
@@ -2,14 +2,55 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // 引入路由组件(import必须在所有业务逻辑之前)
-import Recommend from 'components/recommend/recommend'
-import Rank from 'components/rank/rank'
-import Singer from 'components/singer/singer'
-import Search from 'components/search/search'
-import singerDetail from 'components/singer-detail/singer-detail'
-import Disc from 'components/disc/disc'
-import TopList from 'components/top-list/top-list'
-import UserCenter from 'components/user-center/user-center'
+// import Recommend from 'components/recommend/recommend'
+// import Rank from 'components/rank/rank'
+// import Singer from 'components/singer/singer'
+// import Search from 'components/search/search'
+// import singerDetail from 'components/singer-detail/singer-detail'
+// import Disc from 'components/disc/disc'
+// import TopList from 'components/top-list/top-list'
+// import UserCenter from 'components/user-center/user-center'
+// 利用路由懒加载
+const Recommend = (resolve) => {
+    import('components/recommend/recommend').then(module => {
+        resolve(module)
+    })
+}
+const Rank = (resolve) => {
+    import('components/rank/rank').then(module => {
+        resolve(module)
+    })
+}
+const Singer = (resolve) => {
+    import('components/singer/singer').then(module => {
+        resolve(module)
+    })
+}
+const Search = (resolve) => {
+    import('components/search/search').then(module => {
+        resolve(module)
+    })
+}
+const singerDetail = (resolve) => {
+    import('components/singer-detail/singer-detail').then(module => {
+        resolve(module)
+    })
+}
+const Disc = (resolve) => {
+    import('components/disc/disc').then(module => {
+        resolve(module)
+    })
+}
+const TopList = (resolve) => {
+    import('components/top-list/top-list').then(module => {
+        resolve(module)
+    })
+}
+const UserCenter = (resolve) => {
+    import('components/user-center/user-center').then(module => {
+        resolve(module)
+    })
+}
 // 通过Vue.use()来加载路由
 Vue.use(Router)
 // 导出路由实例,配置路由规则
